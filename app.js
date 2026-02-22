bb8a925cb309b503c0a7ccf3abb36d5a71ecf627
Farhan Sadik <farhansadik703@gmail.com>
2026-02-22 19:25:50 +0600

    project design completed according to figma

---
+++ app.js
@@ -0,0 +1,8 @@
+const interviewList = [];
+const rejectedList = [];
+
+let total = document.getElementById("total");
+let interview = document.getElementById("interview");
+let rejected = document.getElementById("rejected");
+
+console.log(total, interview, rejected);
